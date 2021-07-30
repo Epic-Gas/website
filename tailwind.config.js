9e990593ac19b79405d6e75cb9f839dfb89fa445
Louis Cutner <lcutner@me.com>
2021-07-30 01:28:24 +0100

    added files

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    colors: {
+      white: "#FFFFFF",
+      black: "#000000",
+      "roar-red": "#E20716",
+      "jamie-blue": "#0FB8C3",
+      "tms-purple": "#7428C5",
+    },
+    extend: {
+      transitionProperty: {
+        height: "max-height",
+      },
+      spacing: {
+        100: "60rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
